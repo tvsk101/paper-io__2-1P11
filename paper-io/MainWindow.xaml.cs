d1b14c581c8cfda66ae709c50c1867a8c082e18e
tvsk101 <tvsk101@gmail.com>
2021-11-29 17:27:31 +0300

    Таймер

--- paper-io/MainWindow.xaml.cs
+++ paper-io/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,13 +17,42 @@ using System.Windows.Shapes;
 namespace paper_io
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для paper_io.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerDisplay;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "3";
+            tb.Visibility = Visibility.Visible;
+            int count = Convert.ToInt32(tb.Text);
+            timer.Tag = count;
+            bc.IsEnabled = false;
+            timer.Start();
+        }
+
+        private void TimerDisplay(object sender, EventArgs e)
+        {
+            int count = (int)timer.Tag;
+            tb.Text = (--count).ToString();
+            timer.Tag = count;
+            if (count == 0)
+            {
+                timer.Stop();
+                bc.IsEnabled = true;
+                tb.Visibility = Visibility.Hidden;
+            }
         }
     }
+
 }
